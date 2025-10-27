da1f625145b9318e615e8db8c31f7d21b54189a9
nicholasdoloksaribu <113407564+nicholasdoloksaribu@users.noreply.github.com>
2025-10-27 14:03:45 +0700

    Create CheckAdmin.php
    make middleware checkAdmin

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+    * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(auth()->check() && auth()->user()->role==1){
+            return $next($request);
+        } 
+        return response()->json([
+                'message' => 'You are not admin'
+            ]);
+    }
+}
