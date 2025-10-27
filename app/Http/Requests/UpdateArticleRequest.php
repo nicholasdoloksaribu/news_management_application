c157525527783a61d9f1ef601ec1e10c254eb3fe
nicholasdoloksaribu <113407564+nicholasdoloksaribu@users.noreply.github.com>
2025-10-27 14:04:59 +0700

    Create UpdateArticleRequest.php
    make file UpdateArticleRequest for request validated for update article

---
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'title' => 'sometimes|string',
+            'content'=> 'sometimes|string',
+            'image' => 'sometimes|image|max:2048',
+            'user_id' => 'sometimes|integer|exists:users,id'
+        ];
+    }
+}
